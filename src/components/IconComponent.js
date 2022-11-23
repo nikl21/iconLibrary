061d3eede52c9a5aa49149ef6b7ee5cb4b971406
Nikhil Nalin <nikhil@NH-LT-0020.local>
2022-11-23 16:13:24 +0530

    Change download logic

--- src/components/IconComponent.js
+++ src/components/IconComponent.js
@@ -7,6 +7,27 @@ const IconComponent = ({ name, category, url }) => {
   const downloadImage = () => {
     saveAs(url, name); // Put your image url here.
   };
+  const download = e => {
+    e.preventDefault();
+    console.log(e.target.href);
+    fetch(e.target.href, {
+      method: 'GET',
+      headers: {},
+    })
+      .then(response => {
+        response.arrayBuffer().then(function (buffer) {
+          const url = window.URL.createObjectURL(new Blob([buffer]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', name); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <Box boxSize="200" position="relative">
       <Image src={url} alt="name" />
@@ -20,9 +41,11 @@ const IconComponent = ({ name, category, url }) => {
           bottom="6"
           m={2}
           bg="black"
-          onClick={downloadImage}
+          _hover={{ backgroundColor: 'gray' }}
         >
-          <DownloadIcon color="white" boxSize={4} />
+          <a href={url} download onClick={e => download(e)}>
+            <DownloadIcon color="white" boxSize={4} />
+          </a>
         </Square>
       </Flex>
     </Box>
